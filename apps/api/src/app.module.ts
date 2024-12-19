7f163d68410cb1d01f42218d9647f3a4b3ff46e2
Yasuaki Toyoda <66811920+YodaRoshi@users.noreply.github.com>
2024-12-19 13:40:34 -0500

    create user model and API initialized

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { UsersModule } from './models/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+
+const maxAge = (Number(process.env.MAX_AGE) || 24) * 60 * 60;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: maxAge },
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // Makes the API publicly accessible, allowing dev-tools like Apollo Server and GraphQL Playground to access the schema for auto-complete, visual documentation, and query testing.
+      introspection: true,
+      // Generates GraphQL scheme, required for the code first approach
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // Map TypeScript `number` to GraphQL `Int`
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+      // Apply @UseGuards() globally to all field resolvers( @ResolveField() ) regardless of whetehr @UseGuards() is applied to the parent
+      fieldResolverEnhancers: ['guards'],
+    }),
+    PrismaModule,
+    UsersModule,
+
+    // PrismaModule: Registering a module with @Global() in any module makes it accessible application-wide. However, for clarity and convention, it's typically done in AppModule. Can you make  it even shorter?
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { add } from '@foundation/sample-lib';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(add, add(2, 8));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
